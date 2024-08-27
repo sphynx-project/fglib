be174ac5f35d4a4b4b42ac5522158704d2b6fc75
Kevin Alavik (puffer) <kevin@alavik.se>
2024-08-27 09:00:31 +0200

    ADded license

--- fglib.h
+++ fglib.h
@@ -1,3 +1,12 @@
+/* fglib v1.0 - Freestanding Graphic Library */
+
+#ifndef FGLIB_H
+#define FGLIB_H
+
+// todo
+
+#endif // FGLIB_H
+
 /* MIT License
 
 Copyright (c) 2024 SphynxOS Developers
@@ -19,10 +28,3 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
-
-#ifndef FGLIB_H
-#define FGLIB_H
-
-// todo
-
-#endif // FGLIB_H
